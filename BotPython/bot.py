6e97e61d3b067e5e4f5b6469b0e284d6500b8128
AKA5144 <ar405162@gmail.com>
2025-06-23 15:05:44 +0200

    introduce modularity

--- BotPython/bot.py
+++ BotPython/bot.py
@@ -1,45 +1,39 @@
 import os
-import asyncio
 import discord
 from discord.ext import commands
+from discord import app_commands,Embed
 from dotenv import load_dotenv
-import webbrowser
-
 
 load_dotenv()
 
-# Configuration des intents
 intents = discord.Intents.default()
 intents.message_content = True
-intents.members = True
-intents.presences = True
 
-# Création du bot avec le préfixe "/"
 bot = commands.Bot(command_prefix="/", intents=intents)
 
-# Quand le bot est prêt
 @bot.event
 async def on_ready():
-    print(f"Connected as {bot.user} (ID: {bot.user.id})")
+    print(f"Logged in as {bot.user} (ID: {bot.user.id})")
+    
+    # Supprimer les slash commands pour le GUILD ID donné, ou None pour global
+    await bot.tree.sync(guild=None)  # tu peux mettre un Guild ici si tu veux tester localement
+    print("Slash commands synchronisées.")
 
 @bot.event
 async def on_message(message):
     if message.author.bot:
         return
     if bot.user in message.mentions:
-        await message.channel.send("STOP PINGING ME !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! ")
+        await message.channel.send ("STOP PINGING ME !!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!!! ")
         await message.channel.send("https://media.tenor.com/CWf8Fm-fnT0AAAAM/marisad-marisa-kirisame.gif")
     await bot.process_commands(message)
 
-@bot.command()
-async def openhtml(ctx):
-    chemin_absolu = os.path.abspath("WEBinterface/Main.html")
-    url_locale = f"file:///{chemin_absolu.replace(os.sep, '/')}"
-    webbrowser.open(url_locale)
-    await ctx.send("Le fichier HTML a été ouvert dans le navigateur.")
+@bot.tree.command(name="website", description="get the website")
+async def website(interaction: discord.Interaction):
+    embed = Embed(title="Online Website !", url="https://aka5144.github.io/TouhouCardGame")
+    await interaction.response.send_message(embed=embed)
 
 
-# Lancement du bot avec le token sécurisé
 if __name__ == "__main__":
     token = os.getenv("DISCORD_TOKEN")
     if not token:
