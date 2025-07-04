91576b8822e14f81a6c4fb4b2dbadb496e342673
AKA5144 <ar405162@gmail.com>
2025-07-04 12:26:37 +0200

    test thingies

--- backend/server.js
+++ backend/server.js
@@ -40,9 +40,8 @@ const db = mysql.createConnection({
   user: process.env.DB_USER,
   password: process.env.DB_PASS,
   database: process.env.DB_NAME,
-
+  port: process.env.DB_PORT, // Utiliser le port 44067 de votre capture
   connectTimeout: 30000, // 30 secondes
-  ssl: { rejectUnauthorized: false } // SSL requis par Railway
 });
 
 db.connect((err) => {
