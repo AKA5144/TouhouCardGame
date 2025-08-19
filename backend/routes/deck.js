593104a2082dd1af2c5b3a3bebdc52321b8fa3eb
AKA5144 <ar405162@gmail.com>
2025-08-19 14:03:28 +0200

    aled

--- backend/routes/deck.js
+++ backend/routes/deck.js
@@ -81,7 +81,7 @@ deckRouter.get('/testdb', async (req, res) => {
   try {
     console.log('DB_HOST:', process.env.DB_HOST);
 console.log('DB_USER:', process.env.DB_USER);
-console.log('DB_PASS:', process.env.DB_PASS ? '****' : null);
+console.log('DB_PASS:', process.env.DB_PASS);
 console.log('DB_NAME:', process.env.DB_NAME);
 console.log('DB_PORT:', process.env.DB_PORT);
     const [rows] = await db.query('SELECT 1+1 AS result');
