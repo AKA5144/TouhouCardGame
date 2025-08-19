4881dd683c9a384ff6dfff72da05c6f866dec8b0
AKA5144 <ar405162@gmail.com>
2025-08-19 10:20:01 +0200

    change secure settings

--- backend/routes/discord.js
+++ backend/routes/discord.js
@@ -72,10 +72,11 @@ authRouter.get('/discord/callback', async (req, res) => {
 
     res.cookie('token', token, {
       httpOnly: true,
-      secure: process.env.NODE_ENV === 'production',
-      sameSite: 'lax',
+      secure: true, 
+      sameSite: 'none', 
       maxAge: 7 * 24 * 60 * 60 * 1000,
     });
+
 
 
 
